75c05d9d585a1e0161116ce09d8f63efab36bc65
SosaMati <matiasezequielsosa@gmail.com>
2023-08-08 20:53:10 -0300

    add pages

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,39 +1,75 @@
-import Work1 from '../../assets/yo.jpg'
-import Work2 from '../../assets/yo.jpg'
-import Work3 from '../../assets/yo.jpg'
-import Work4 from '../../assets/yo.jpg'
-import Work5 from '../../assets/yo.jpg'
-import Work6 from '../../assets/yo.jpg'
+import Work1 from '../../assets/work1.png'
+import Work2 from '../../assets/work2.png'
+import Work3 from '../../assets/work3.png'
+import Work4 from '../../assets/work4.png'
+import Work5 from '../../assets/work5.png'
+import Work6 from '../../assets/work6.png'
+import Work7 from '../../assets/work7.png'
+import Work8 from '../../assets/work8.png'
+import Work9 from '../../assets/work9.png'
 
 export const projectsData = [
   {
     id: 1,
     image: Work1,
-    title: 'laksl'
+    title: 'Todo App TS',
+    demoLink: 'https://todo-type-script-ten.vercel.app/',
+    repoLink: 'https://github.com/SosaMati/todoTypeScript'
+  },
+  {
+    id: 9,
+    image: Work9,
+    title: 'Shopping Cart',
+    demoLink: 'https://shopping-cart-rose-tau.vercel.app/',
+    repoLink: 'https://github.com/SosaMati/shopping-cart'
   },
   {
     id: 2,
     image: Work2,
-    title: 'laksl'
+    title: 'Heroes App',
+    demoLink: 'https://heroes-app-psi.vercel.app/',
+    repoLink: 'https://github.com/SosaMati/Heroes-App'
   },
   {
     id: 3,
     image: Work3,
-    title: 'laksl'
+    title: 'Citas',
+    demoLink: 'https://citas-react-vite-gold.vercel.app/',
+    repoLink: 'https://github.com/SosaMati/citas-react-vite'
   },
   {
     id: 4,
     image: Work4,
-    title: 'laksl'
+    title: 'Control de gastos',
+    demoLink: 'https://control-presupuesto-neon.vercel.app/',
+    repoLink: 'https://github.com/SosaMati/control-presupuesto'
   },
   {
     id: 5,
     image: Work5,
-    title: 'laksl'
+    title: 'Gifs Search App',
+    demoLink: 'https://react-giftsearchapp.vercel.app/',
+    repoLink: 'https://github.com/SosaMati/react-giftsearchapp'
   },
   {
     id: 6,
     image: Work6,
-    title: 'laksl'
+    title: 'Cotillón App',
+    demoLink: 'https://lahormiguitaviajera-self.vercel.app/',
+    repoLink: 'https://github.com/SosaMati/cotillon'
+  },
+  {
+    id: 7,
+    image: Work7,
+    title: 'Cotizador Criptomonedas',
+    demoLink: 'https://cotizador-criptomonedas-chi.vercel.app/',
+    repoLink: 'https://github.com/SosaMati/cotizador-criptomonedas'
+  },
+  {
+    id: 8,
+    image: Work8,
+    title: 'Buscador de Películas',
+    demoLink: 'https://buscador-peliculas-phi.vercel.app/',
+    repoLink: 'https://github.com/SosaMati/buscadorPeliculas'
   },
 ];
--- src/components/work/WorkItems.jsx
+++ src/components/work/WorkItems.jsx
@@ -5,10 +5,10 @@ const WorkItems = ({ item }) => {
       <img src={item.image} alt={item.title} className="work__img" />
       <h3 className="work__title">{item.title}</h3>
       <div className="work__links">
-        <a href="#" className="work__button">
+        <a href={item.demoLink} target="_blank" rel="noreferrer" className="work__button">
           Demo <i className="bx bx-right-arrow-alt work__button-icon"></i>
         </a>
-        <a href="#" className="work__button">
+        <a href={item.repoLink} target="_blank" rel="noreferrer" className="work__button">
           Repo <i className="bx bx-right-arrow-alt work__button-icon"></i>
         </a>
       </div>
